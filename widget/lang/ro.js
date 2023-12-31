6a5a8e058a8314170f2184796829aee1e089b0fb
Robert Csaszar <robert.csaszar@dowdian.com>
2023-12-31 13:15:54 +0200

    add Romanian

---
+++ widget/lang/ro.js
@@ -0,0 +1,20 @@
+export default {
+  powered_by: 'Comentarii oferite de Cusdis',
+  post_comment: 'Comentează',
+  loading: 'Se încarcă...',
+
+  // comment
+  email: 'Email (opțional)',
+  nickname: 'Nume',
+  reply_placeholder: 'Răspunde...',
+  reply_btn: 'Răspunde',
+  sending: 'Se trimite',
+
+  // reply
+  mod_badge: 'MOD',
+  content_is_required: 'Conținutul este obligatoriu',
+  nickname_is_required: 'Numele este obligatoriu',
+
+  comment_has_been_sent:
+    'Comentariul tău a fost trimis. Te rog așteaptă aprobarea.',
+}
